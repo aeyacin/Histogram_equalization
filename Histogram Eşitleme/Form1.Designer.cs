6caf65db0cf1b51da61a3bb8e45559fffda64c44
aeyacin <aliekberyacin.net.tc@gmail.com>
2014-05-03 16:36:09 +0300

    Histogram equalization - Histogram eşitleme
    Histogram equalization
    C# ile Histogram Eşitleme

---
+++ Histogram Eşitleme/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Histogram_Eşitleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(643, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Histogram Eşitleme";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 333);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.Location = new System.Drawing.Point(329, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(286, 333);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(755, 384);
+            this.shapeContainer1.TabIndex = 3;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 312;
+            this.lineShape1.X2 = 312;
+            this.lineShape1.Y1 = 8;
+            this.lineShape1.Y2 = 375;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(643, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Histogram Eşitleme Uygula";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(637, 201);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 94);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(643, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 36);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Resim Aç";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 384);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "Histagram eşitleme";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Histogram Eşitleme/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace Histogram_Eşitleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+       
+        int[] HistogramRed=new int[256];
+        int[] HistogramGreen = new int[256];
+        int[] HistogramBlue = new int[256];
+        int[] HistogramRedK = new int[256];
+        int[] HistogramGreenK = new int[256];
+        int[] HistogramBlueK = new int[256];
+        int[] YüzdelikRed=new int[256];
+        int[] YüzdelikGreen = new int[256];
+        int[] YüzdelikBlue = new int[256];
+        int Çözünürlük;
+
+        String a = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            //   string[] karakter = new string[8];
+            OpenFileDialog resimsec = new OpenFileDialog();
+
+            resimsec.Title = "Lütfen Bir Gri Resim Seçiniz";
+            resimsec.Filter = " (*.jpg)|*.jpg|(*.png)|*.png";
+            if (resimsec.ShowDialog() == DialogResult.OK)
+            {
+
+                var orjinalGoruntu = new Bitmap(resimsec.OpenFile());
+                this.pictureBox1.Image = orjinalGoruntu;
+                var goruntuGenislik = orjinalGoruntu.Width;
+                var goruntuYukseklik = orjinalGoruntu.Height;
+                Çözünürlük = goruntuGenislik * goruntuYukseklik;
+
+                var outputmap = new Bitmap(goruntuGenislik, goruntuYukseklik);
+              
+                for (var i = 0; i < goruntuGenislik; i++)
+                {
+                    for (var j = 0; j < goruntuYukseklik; j++)
+                    {
+                        var piksel = orjinalGoruntu.GetPixel(i, j);
+
+                        HistogramRed[(int)piksel.R]++;
+                        HistogramGreen[(int)piksel.G]++;
+                        HistogramBlue[(int)piksel.B]++;
+                       
+                    }
+                }
+
+
+
+
+                HistogramRedK[0] = HistogramRed[0];
+                HistogramGreenK[0] = HistogramGreen[0];
+                HistogramBlueK[0] = HistogramBlue[0];
+
+                for (int j = 0; j < 255; j++)
+                {
+                    HistogramRedK[j + 1] += HistogramRed[j];
+                    HistogramGreenK[j + 1] += HistogramGreen[j];
+                    HistogramBlueK[j + 1] += HistogramBlue[j];
+                }
+
+                for (int j = 0; j < 256; j++)
+                {
+                    HistogramRedK[j] = ((int)HistogramRedK[j] * 255) / (Çözünürlük);
+                    HistogramGreenK[j] = ((int)HistogramGreenK[j] * 255) / (Çözünürlük);
+                    HistogramBlueK[j] = ((int)HistogramBlueK[j] * 255) / (Çözünürlük);
+                }
+
+
+                for (int i = 0; i < goruntuGenislik; i++)
+                {
+                    for (int j = 0; j < goruntuYukseklik; j++)
+                    {
+                        
+                        var piksel2 = orjinalGoruntu.GetPixel(i, j);
+                
+
+var renkliPiksel1 = Color.FromArgb((int)(HistogramRedK[(int)(piksel2.R)]), (int)(HistogramGreenK[piksel2.G]), (int)(HistogramRedK[piksel2.B]));
+                        /*
+                        outputmap.pixels[i][j].red = histogramredk[(int)kaynak.pixels[i][j].red];
+                        outputmap.pixels[i][j].green = histogramgreenk[(int)kaynak.pixels[i][j].green];
+                        outputmap.pixels[i][j].blue = histogrambluek[(int)kaynak.pixels[i][j].blue];
+                        */
+                        outputmap.SetPixel(i,j,(renkliPiksel1));
+                    }
+                }
+
+/*
+
+                for (int f = 0; f < 256; f++)
+                {
+                    Yüzdelik[f] =( S[f]*10000) / Çözünürlük;
+                    a += Yüzdelik[f].ToString() + "-";
+                    //  a = Çözünürlük.ToString();
+                }
+
+                for (var i = 0; i < goruntuGenislik; i++)
+                {
+                    for (var j = 0; j < goruntuYukseklik; j++)
+                    {
+                        var piksel2 = orjinalGoruntu.GetPixel(i, j);
+                            int pixel;
+                        for (int f = 0; f < 256; f++)
+                        {
+                            
+                            if (S[f] == 0)
+                            {
+                                pixel= Yüzdelik[f]/10000;
+                            }
+                            else if (S[f] >= 255)
+                            {
+                                pixel = 255;
+                            }
+                            else if(f > 0)
+                            {
+                                pixel = S[f - 1] + (Yüzdelik[f]/10000); 
+                            }
+                            
+                        }
+                            
+                        var renkliPiksel1 = Color.FromArgb((int)(piksel2.R), (int)(piksel2.R), (int)(piksel2.R));
+
+                                           
+
+                        piksellestirilmisGoruntu1.SetPixel(i, j, renkliPiksel1);
+
+                    }
+                }
+*/
+                pictureBox2.Image = outputmap;
+               
+              //  MessageBox.Show(a);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+                var orjinalGoruntu = new Bitmap(pictureBox1.Image);
+
+
+
+                pictureBox2.Image = histogramEşitleme(orjinalGoruntu);
+
+
+
+            
+        }
+
+        public Bitmap histogramEşitleme(Bitmap KaynakResim)
+        {
+            Bitmap renderedImage = KaynakResim;
+
+            uint pixels = (uint)renderedImage.Height * (uint)renderedImage.Width;
+            decimal Const = 255 / (decimal)pixels;
+
+            int x, y, R, G, B;
+
+          
+            int[] HistogramRed2 = new int[256];
+            int[] HistogramGreen2 = new int[256];
+            int[] HistogramBlue2 = new int[256];
+
+
+            for (var i = 0; i < renderedImage.Width; i++)
+            {
+                for (var j = 0; j < renderedImage.Height; j++)
+                {
+                    var piksel = renderedImage.GetPixel(i, j);
+
+                    HistogramRed2[(int)piksel.R]++;
+                    HistogramGreen2[(int)piksel.G]++;
+                    HistogramBlue2[(int)piksel.B]++;
+
+                }
+            }
+
+            int[] cdfR = HistogramRed2;
+            int[] cdfG = HistogramGreen2;
+            int[] cdfB = HistogramBlue2;
+           
+            for (int r = 1; r <= 255; r++)
+            {
+                cdfR[r] = cdfR[r] + cdfR[r - 1];
+                cdfG[r] = cdfG[r] + cdfG[r - 1];
+                cdfB[r] = cdfB[r] + cdfB[r - 1];
+            }
+
+            for (y = 0; y < renderedImage.Height; y++)
+            {
+                for (x = 0; x < renderedImage.Width; x++)
+                {
+                    Color pixelColor = renderedImage.GetPixel(x, y);
+
+                    R = (int)((decimal)cdfR[pixelColor.R] * Const);
+                    G = (int)((decimal)cdfG[pixelColor.G] * Const);
+                    B = (int)((decimal)cdfB[pixelColor.B] * Const);
+
+                    Color newColor = Color.FromArgb(R, G, B);
+                    renderedImage.SetPixel(x, y, newColor);
+                }
+            }
+            return renderedImage;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog resimsec = new OpenFileDialog();
+
+            resimsec.Title = "Lütfen Bir Gri Resim Seçiniz";
+            resimsec.Filter = " (*.jpg)|*.jpg|(*.png)|*.png";
+            if (resimsec.ShowDialog() == DialogResult.OK)
+            {
+
+                var orjinalGoruntu = new Bitmap(resimsec.OpenFile());
+                var orj = orjinalGoruntu;
+                this.pictureBox1.Image = orj;
+                var goruntuGenislik = orjinalGoruntu.Width;
+                var goruntuYukseklik = orjinalGoruntu.Height;
+                Çözünürlük = goruntuGenislik * goruntuYukseklik;
+
+
+            //    pictureBox1.Image = histogramEqualization(orjinalGoruntu);
+
+
+            }
+        }
+        }
+   
+}
